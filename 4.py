a88395657a4f7068b4f14602c9cdf262caf7504b
Alexandr <olexandrzhezheria@gmail.com>
2022-06-30 19:30:31 +0300

    Проба

--- 4.py
+++ 4.py
@@ -11,26 +11,18 @@ class Car:
         self.model_name = model_name
         self.price = price
 
+
 car1 = Car('Honda', 'Jazz', 900000)
 car2 = Car('Suzuki', 'Alto', 450000)
 car3 = Car('BMW', 'X5', 9000000)
 
+print(f"1 - {car1}  \n2 - {car2}  \n3 - {car3}")
+
+
 class Salon(Car):
+    cars = [car1, car2, car3]
 
-    def __init__(self):
-        cars = []
-        cars.append(x)
+    x = input("Выберите машину для покупки: ")
 
-        print(f"1 - {car1}  /n 2 - {car2}  /n 3 - {car3}")
-
-        x = input("Выберите машину для покупки: ")
+    if x in cars:
         print(f"Поздравляем! Вы купили {x} С Вас {self.price} грн.")
-
-    # def car_list(self):
-    #     self.car_list()
-Salon(Car)
-
-
-
-
-
